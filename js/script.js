939d05d5b871032beef285fadd25bdaf954e9b08
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-10-17 18:38:18 +0200

    added condition

--- js/script.js
+++ js/script.js
@@ -12,38 +12,43 @@
         let userWord = inputEl.value;
         let reversed = "";
         let msg;
-    
-        //prendo la parola e la riverto tramite funzione, ritornerà la parola reversata
-        takeAndRevert(userWord, reversed);
-        
-        // creo la condizione per verificare se la parola è palindroma
-        message(userWord,reversed)
-    
-        // stampo i risultati 
-        resultPrint(userWord,reversed,msg)
-    
+
+        if (userWord !== "") {
+            //prendo la parola e la riverto tramite funzione, ritornerà la parola reversata
+            takeAndRevert(userWord, reversed);
+
+            // creo la condizione per verificare se la parola è palindroma
+            message(userWord, reversed)
+
+            // stampo i risultati 
+            resultPrint(userWord, reversed, msg)
+        } else {
+            msg = "Inserisci una parola corretta!!"
+            resultPrint (msg)
+        }
+
         //FUNCTIONS
-        function takeAndRevert(word1, word2){
+        function takeAndRevert(word1, word2) {
             for (let i = userWord.length - 1; i >= 0; i--) {
                 reversed += userWord[i]
             }
         }
-        function message(mess1,mess2) {
+        function message(mess1, mess2) {
             if (userWord === reversed) {
                 msg = "La tua parola è Palindroma!"
-                } else {
+            } else {
                 msg = "La tua parola non è Palindroma!"
-                }
+            }
         }
         function resultPrint(word1, word2, message) {
             const alertEl = document.querySelector(".alert");
-        alertEl.classList.remove("d-none")
-        alertEl.innerHTML = 
-        `
+            alertEl.classList.remove("d-none")
+            alertEl.innerHTML =
+                `
         ${userWord}
         ${reversed}
         ${msg}
-        ` 
+        `
         }
     }
 })()
