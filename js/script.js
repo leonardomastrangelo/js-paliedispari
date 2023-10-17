8adc4f0d13308ee1eb6a5cb01243c36ae3737a6a
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-10-17 15:52:58 +0200

    undefined problem

--- js/script.js
+++ js/script.js
@@ -10,19 +10,11 @@ const inputEl = document.getElementById("data");
 btn.addEventListener("click", function () {
     // catturo il dato dell'user in una variabile
     let userWord = inputEl.value;
-    // creo un array dove inserire le singole lettere della parola
-    const userArray = []
-    // creo un ciclo for dedicato a dividere la parola in singole lettere e pusharle nell'array
-    for (let i = 0; i < userWord.length; i++) {
-        const letter = userWord[i];
-        userArray.push(letter)
+    let reversed = "";
+    for (let i = userWord.length; i >= 0; i--) {
+       reversed += userWord[i]
+       console.log(reversed);
     }
-    //creo la condizione per verificare se la parola è palindroma
-    let msg;
-    if (userArray === userArray.reverse()) {
-        msg = "La tua parola è polindroma!"
-    } else {
-        msg ="La tua parola non è Polindroma!"
-    }
-    console.log(userArray.reverse());
+    
+    
 })
