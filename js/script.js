ee9a312263d439830794500c013a2ec7757c9453
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-10-17 16:08:23 +0200

    fundaments of palindroma exercise

--- js/script.js
+++ js/script.js
@@ -10,11 +10,25 @@ const inputEl = document.getElementById("data");
 btn.addEventListener("click", function () {
     // catturo il dato dell'user in una variabile
     let userWord = inputEl.value;
+    //creo un ciclo per reversare la parola dell'user
     let reversed = "";
-    for (let i = userWord.length; i >= 0; i--) {
+    for (let i = userWord.length - 1; i >= 0; i--) {
        reversed += userWord[i]
-       console.log(reversed);
     }
-    
-    
+    // creo la condizione per verificare se la parola è palindroma
+    let msg;  
+    if (userWord === reversed) {
+    msg = "La tua parola è Palindroma!"
+    } else {
+    msg = "La tua parola non è Palindroma!"
+    }
+    // stampo i risultati 
+    const alertEl = document.querySelector(".alert");
+    alertEl.classList.remove("d-none")
+    alertEl.innerHTML = 
+    `
+    ${userWord}
+    ${reversed}
+    ${msg}
+    `  
 })
